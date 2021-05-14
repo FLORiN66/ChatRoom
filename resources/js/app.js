dd467d50a3b06712941d9f63f27fbc4c820fd1d9
Florin Moisei <moiseiflorin66@gmail.com>
2021-05-14 11:17:48 +0300

    Chat room

---
+++ resources/js/app.js
@@ -0,0 +1,28 @@
+import './bootstrap';
+
+window.Vue = require('vue').default;
+import VueChatScroll from 'vue-chat-scroll';
+
+Vue.use(VueChatScroll);
+
+Vue.component('message', require('./components/message.vue').default);
+
+
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: '',
+        chat: {
+            message: []
+        }
+    },
+    methods: {
+        send() {
+            if (this.message.length != 0) {
+                this.chat.message.push(this.message);
+                this.message = '';
+            }
+        }
+    }
+});
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+
+	<link rel="stylesheet" href=" {{asset('css/app.css')}} ">
+	<link rel="bootstrap" href=" {{asset('bootstrap/')}} ">
+	<style>
+		.list-group {
+			overflow-y: scroll;
+			height: 200px;
+		}
+	</style>
+</head>
+
+<body>
+	<div class="container">
+		<div class="row d-flex justify-content-center" id="app">
+			<div class="offser-4 col-4">
+				<li class="list-group-item active">Chat Room</li>
+				<ul class="list-group" v-chat-scroll>
+					<message v-for="value in chat.message" :key="value.index">
+						@{{value}}
+					</message>
+				</ul>
+				<input type="text" class="form-control" placeholder="Type a message" v-model="message" @keyup.enter='send'>
+			</div>
+		</div>
+	</div>
+
+	<sript src="{{asset('js/app.js')}}"></sript>
+	<script src="{{ mix('/js/app.js') }}"></script>
+</body>
+
+</html>
