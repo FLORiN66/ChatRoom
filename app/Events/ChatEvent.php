76e8d8c33d7b30abe41e1f38976930c20e0a917b
FLORiN66 <moiseiflorin66@gmail.com>
2021-05-14 18:13:42 +0300

    Broadcast config

--- app/Events/ChatEvent.php
+++ app/Events/ChatEvent.php
@@ -9,19 +9,24 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\User;
 
-class ChatEvent
+class ChatEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+    public $user;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($message, User $user)
     {
-        //
+        return $this->message = $message;
+        return $this->user = $user;
     }
 
     /**
@@ -31,6 +36,6 @@ class ChatEvent
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new PrivateChannel('chat');
     }
 }
