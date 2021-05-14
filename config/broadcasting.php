288a9e3986246e10d9776db9282e8e6795c02dc1
FLORiN66 <moiseiflorin66@gmail.com>
2021-05-14 18:06:10 +0300

    add Events and Listeners

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -32,9 +32,9 @@ return [
 
         'pusher' => [
             'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
+            'key' => env('any_key'),
+            'secret' => env('any_secret'),
+            'app_id' => env('any_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
